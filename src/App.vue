e01d375c796de4fd6f1a8f51e811223868cdfe4d
Aleksey Pravdin <leks16@bk.ru>
2021-07-13 17:31:00 +0300

    hash

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,44 @@
         <header>
             <h1>Мои расходы</h1>
         </header>
+        <div class="menu">
+            <a href="#dashboard">Dashboard</a> /
+            <a href="#about">About</a> /
+            <a href="#notfound">Not Found</a>
+        </div>
         <main>
+            <div class="content-page">
+                <About v-if="page === 'about'"/>
+                <Dashboard v-if="page === 'dashboard'"/>
+                <NotFound v-if="page === 'notfound'"/>
+            </div>
             <PopUp @addNewPayment="addData"/>
             <br>
             <PaymentsDisplay :list="paymentsList"></PaymentsDisplay>
         </main>
         Общая стоимость: {{ getFPV }}
-<!--        <CategorySelect :category="category"/>-->
+        <!--        <CategorySelect :category="category"/>-->
     </div>
 </template>
 <script>
 import PaymentsDisplay from "./components/PaymentsDisplay";
 import PopUp from "./components/PopUp";
-import { mapMutations, mapGetters, mapActions } from "vuex";
+
+import About from "./views/About";
+import Dashboard from "./views/Dashboard";
+import NotFound from "./views/NotFound";
+
+import {mapMutations, mapGetters, mapActions} from "vuex";
 // import CategorySelect from "./components/CategorySelect";
 
 export default {
     name: 'App',
-    components: {PopUp, PaymentsDisplay,},
+    components: {PopUp, PaymentsDisplay, About, Dashboard, NotFound},
+    data() {
+        return {
+            page: ''
+        }
+    },
     methods: {
         ...mapMutations([
             'setPaymentListData',
@@ -34,25 +54,9 @@ export default {
             this.addDataToPaymentsList(data)
             // this.paymentsList.push(data)
         },
-        // fetchData() {
-        //     return [
-        //         {
-        //             date: '28.02.2020',
-        //             category: 'Food',
-        //             value: 169
-        //         },
-        //         {
-        //             date: '20.04.2021',
-        //             category: 'Sport',
-        //             value: 400
-        //         },
-        //         {
-        //             date: '28.05.2020',
-        //             category: 'Internet',
-        //             value: 200
-        //         }
-        //     ]
-        // }
+        setPage() {
+            this.page = location.hash.slice(1)
+        }
     },
     computed: {
         ...mapGetters({
@@ -68,14 +72,19 @@ export default {
     },
     created() {
         this.fetchData()
-        if(!this.category.length) {
+        if (!this.category.length) {
             this.fetchCategory()
         }
         // this.$store.dispatch('fetchData')
         // this.setPaymentListData(this.fetchData())
         // this.paymentsList = this.fetchData()
+    },
+    mounted() {
+        this.setPage()
+        window.addEventListener('hashchange', () => {
+            this.setPage()
+        })
     }
-
 }
 </script>
 
