cd21509aca95d14fcfa962c076da222cd15700d3
Aleksey Pravdin <leks16@bk.ru>
2021-07-14 21:38:13 +0300

    повторение кода по уроку

--- src/App.vue
+++ src/App.vue
@@ -4,41 +4,56 @@
             <h1>Мои расходы</h1>
         </header>
         <div class="menu">
-            <a href="#dashboard">Dashboard</a> /
-            <a href="#about">About</a> /
-            <a href="#notfound">Not Found</a>
+            <router-link to="/dashboard">Dashboard</router-link> /
+            <router-link to="/about">About</router-link> /
+            <router-link to="/notfound">Not Found</router-link>
+            <!--            <a href="dashboard">Dashboard</a> /-->
+            <!--            <a href="about">About</a> /-->
+            <!--            <a href="notfound">Not Found</a>-->
         </div>
         <main>
             <div class="content-page">
-                <About v-if="page === 'about'"/>
-                <Dashboard v-if="page === 'dashboard'"/>
-                <NotFound v-if="page === 'notfound'"/>
+                <router-view/>
+                <!--                <About v-if="page === 'about'"/>-->
+                <!--                <Dashboard v-if="page === 'dashboard'"/>-->
+                <!--                <NotFound v-if="page === 'notfound'"/>-->
             </div>
             <PopUp @addNewPayment="addData"/>
             <br>
-            <PaymentsDisplay :list="paymentsList"></PaymentsDisplay>
+            <PaymentsDisplay :list="currentElements"></PaymentsDisplay>
         </main>
         Общая стоимость: {{ getFPV }}
         <!--        <CategorySelect :category="category"/>-->
+        <Pagination :cur="curPage" :n="n" :length="paymentsList.length" @paginate="onChangePage"/>
     </div>
 </template>
 <script>
 import PaymentsDisplay from "./components/PaymentsDisplay";
 import PopUp from "./components/PopUp";
+import Pagination from "./components/Pagination";
 
-import About from "./views/About";
-import Dashboard from "./views/Dashboard";
-import NotFound from "./views/NotFound";
+// import About from "./views/About";
+// import Dashboard from "./views/Dashboard";
+// import NotFound from "./views/NotFound";
 
 import {mapMutations, mapGetters, mapActions} from "vuex";
 // import CategorySelect from "./components/CategorySelect";
 
 export default {
     name: 'App',
-    components: {PopUp, PaymentsDisplay, About, Dashboard, NotFound},
+    components: {
+        PopUp,
+        PaymentsDisplay,
+        Pagination,
+        // About,
+        // Dashboard,
+        // NotFound
+    },
     data() {
         return {
-            page: ''
+            page: '',
+            curPage: 1,
+            n: 5
         }
     },
     methods: {
@@ -54,15 +69,22 @@ export default {
             this.addDataToPaymentsList(data)
             // this.paymentsList.push(data)
         },
-        setPage() {
-            this.page = location.hash.slice(1)
+        onChangePage(p) {
+            this.curPage = p
         }
+        // setPage() {
+        //     this.page = location.pathname.slice(1)
+        // }
     },
     computed: {
         ...mapGetters({
             paymentsList: 'getPaymentList',
             category: 'getCategoryList'
         }),
+        currentElements() {
+            const {n, curPage } = this
+            return this.paymentsList.slice(n * (curPage - 1), n * (curPage - 1) + n)
+        },
         getFPV() {
             return this.$store.getters.getFullPaymentValue
         },
@@ -79,12 +101,18 @@ export default {
         // this.setPaymentListData(this.fetchData())
         // this.paymentsList = this.fetchData()
     },
-    mounted() {
-        this.setPage()
-        window.addEventListener('hashchange', () => {
-            this.setPage()
-        })
-    }
+    // mounted() {
+    //     this.setPage()
+    //     const links = document.querySelectorAll('a')
+    //     links.forEach(link => {
+    //         link.addEventListener('click', e => {
+    //             e.preventDefault()
+    //             history.pushState({}, "", link.href)
+    //             this.setPage()
+    //         })
+    //     })
+    //     window.addEventListener('popstate', this.setPage)
+    // }
 }
 </script>
 
@@ -98,4 +126,5 @@ export default {
   margin-left: 120px;
   margin-right: 120px;
 }
+
 </style>
---
+++ src/components/Pagination.vue
@@ -0,0 +1,52 @@
+<template>
+    <div :class="[$style.wrp]">
+        <div @click="onClick(cur - 1)"><a href="#">-</a></div>
+        <div
+                v-for="i in amount" :key="i"
+                :class="{
+        [$style.active]: cur === i
+      }"
+                @click="onClick(i)"
+        ><a class="links" href="#">{{ i }}</a></div>
+        <div @click="onClick(cur + 1)"><a class="links" href="#">+</a></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        length: Number,
+        n: Number,
+        cur: Number
+    },
+    computed: {
+        amount () {
+            return Math.ceil(this.length / this.n)
+        }
+    },
+    methods: {
+        onClick (p) {
+            if (p < 1 || p > this.amount) {
+                return
+            }
+            this.$emit('paginate', p)
+        }
+    }
+}
+</script>
+
+<style module lang="scss">
+.wrp {
+  display: flex;
+  & > div {
+    padding: 10px;
+    &.active {
+      background: #b9c9fe;
+    }
+  }
+}
+a {
+  color: #039;
+  text-decoration: none;
+}
+</style>
